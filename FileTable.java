5db4d2b5af2fb2adcc74dfad2b26de03ee4a32c8
Joe Ma <hokyinma@uw.edu>
2017-06-04 18:32:45 -0700

    Commit the filetable class

--- FileTable.java
+++ FileTable.java
@@ -6,11 +6,6 @@ import java.util.Vector;
  */
 public class FileTable {
 
-    private static final int UNUSED = 0;
-    private static final int USED = 1;
-    private static final int READ = 2;
-    private static final int WRITE = 3;
-
     private Vector<FileTableEntry> table;         // the actual entity of this file table
     private Directory dir;        // the root directory
 
@@ -26,6 +21,77 @@ public class FileTable {
         // increment this inode's count
         // immediately write back this inode to the disk
         // return a reference to this file (structure) table entry
+
+        short iNumber; // inode number
+        Inode inode; // Inode reference
+
+        while (true) {
+            // get the inumber form the directory method
+            iNumber = (filename.equals("/") ? (short) 0 : dir.namei(filename));
+
+            // if the Inode for the given file exist
+            if (iNumber >= 0) {
+                inode = new Inode(iNumber);
+
+                // if the file is requesting for reading
+                if (mode.equals("r")) {
+
+                    // and its flag is read or used or unused
+                    if (inode.flag == inode.READ || inode.flag == inode.USED || inode.flag == inode.UNUSED) {
+
+                        // change the flag of the node to read
+                        inode.flag = inode.READ;
+
+                        // No need to wait by breaking the loop directly
+                        break;
+
+                    // if the file is already written by another thread
+                    } else if (inode.flag == inode.WRITE) {
+
+                        //  wait until finish
+                        try {
+                            wait();
+                        } catch (InterruptedException ignored) {
+
+                        }
+                    }
+
+                // if the file is requested for writing / append
+                } else {
+
+                    // the flag of that file is marked as "used" or "unused"
+                    if (inode.flag == inode.USED || inode.flag == inode.UNUSED) {
+
+                        // Change the flag to write
+                        inode.flag = inode.WRITE;
+                        break;
+
+                    } else {
+
+                        // otherwise wait until finish
+                        try {
+                            wait();
+                        } catch (InterruptedException ignored) { }
+                    }
+                }
+            } else if (!mode.equals("r")) {
+                // if the node for the given file does not exist,
+                // create a new inode for that file, use the alloc function from directory to get the inumber
+                iNumber = dir.ialloc(filename);
+                inode = new Inode(iNumber);
+                inode.flag = inode.WRITE;
+                break;
+            } else {
+                return null;
+            }
+        }
+
+
+        inode.count++;
+        inode.toDisk(iNumber);
+        FileTableEntry entry = new FileTableEntry(inode, iNumber, mode);
+        table.addElement(entry);
+        return entry;
     }
 
     public synchronized boolean ffree(FileTableEntry tableEntry) {
@@ -48,11 +114,11 @@ public class FileTable {
         Inode inode = new Inode(tableEntry.iNumber);
 
         if (inode.count == 0) {
-            inode.flag = UNUSED;
+            inode.flag = inode.UNUSED;
         }
 
         // notify waiting threads
-        if (inode.flag == READ || inode.flag == WRITE)
+        if (inode.flag == inode.READ || inode.flag == inode.WRITE)
             notify();
 
         if (inode.count > 0) {
